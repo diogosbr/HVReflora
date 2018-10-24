86c593b2a686506ce74fcc11651c1fd3bda31673
Diogo <diogosbr@gmail.com>
2018-10-24 12:44:36 -0300

    Ok

--- R/image_hv.R
+++ R/image_hv.R
@@ -4,7 +4,7 @@
 #' @param scientific.name A character string with the scientific name.
 #' @param tx Multiplication rate of standard image size  (70 x 150 px).
 #' @param destfolder A character string with the folder where the images will be saved
-#' @param epi A character string with the specific epithet name.
+#' @param municipality A character string with the municipality name. If is NULL (default) download images of all available municipalities.
 #'
 #' @import dplyr
 #' @import httr
@@ -14,7 +14,7 @@
 #' @importFrom utils download.file 
 #' @importFrom utils setTxtProgressBar
 #' @importFrom utils txtProgressBar
-#' @seealso \link{'http://reflora.jbrj.gov.br/reflora/herbarioVirtual'}
+#' @seealso (<http://reflora.jbrj.gov.br/reflora/herbarioVirtual/ConsultaPublicoHVUC/ConsultaPublicoHVUC.do>)
 #' @return A rasterStack with the minimun, maximum, median, mean and standard
 #' @examples
 #' image_hv("Prepusa montana")
@@ -28,17 +28,17 @@ image_hv = function(scientific.name,
   
   if (!is.null(scientific.name)) {
     name = scientific.name %>% 
-      strsplit(" ") %>% 
-      unlist()
+      strsplit(., " ") %>% 
+      unlist(.)
     gen = name[1]
     epi = name[2]
     url_sp = paste0(url, "&nomeCientifico=", paste0(gen, "+", epi))
     if(!is.null(municipality)){
       mun  = municipality %>% 
-        replace_non_ascii() %>% 
-        strsplit(" ") %>% 
-        unlist() %>% 
-        paste(collapse = "+")
+        replace_non_ascii(.) %>% 
+        strsplit(., " ") %>% 
+        unlist(.) %>% 
+        paste(., collapse = "+")
       url_sp = paste0(url, "&nomeCientifico=", paste0(gen, "+", epi), "&municipio=", mun)
     }
   } else{
@@ -46,16 +46,15 @@ image_hv = function(scientific.name,
   }
   
   cdg_html <- url_sp %>%
-    httr::GET() %>%
-    httr::content('text', encoding = 'UTF-8') %>%
-    xml2::read_html() %>%
-    html_nodes('img') %>%
-    html_attr('src')
+    httr::GET(.) %>%
+    httr::content(.,'text', encoding = 'UTF-8') %>%
+    xml2::read_html(.) %>%
+    html_nodes(.,'img') %>%
+    html_attr(.,'src')
   
   if(length(cdg_html)==86){stop("Please check if the taxon name is correct.")}
     
   cdg_html = grep(pattern = "width=", cdg_html, value = T)
-  cdg_html
   
   if (!dir.exists(destfolder)) {
     dir.create(destfolder)
@@ -72,11 +71,11 @@ image_hv = function(scientific.name,
     setTxtProgressBar(pb, i)
     #nomes = unlist(strsplit(cdg_html[i], split = "&"))
     nomes = cdg_html[i] %>% 
-      strsplit(split = "&") %>% 
-      unlist()
+      strsplit(., split = "&") %>% 
+      unlist(.)
     
-    w = as.numeric(nomes[3:4] %>% strsplit(split="=") %>% unlist() %>% .[2])
-    h = as.numeric(nomes[3:4] %>% strsplit(split="=") %>% unlist() %>% .[4])
+    w = as.numeric(nomes[3:4] %>% strsplit(., split="=") %>% unlist(.) %>% .[2])
+    h = as.numeric(nomes[3:4] %>% strsplit(., split="=") %>% unlist(.) %>% .[4])
     
     url1 = paste0(nomes[1:2], collapse = "&")
     url_image = paste(url1, "&", "width=", w * tx, "&", "height=", h * tx, sep = "")
